4848d2698572604e10face55ec7e6b7188689f4d
Sujeong Park <s2017s08@e-mirim.hs.kr>
2019-06-25 11:49:59 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3607 박수정

--- BeautyForestAgent/Form1.cs
+++ BeautyForestAgent/Form1.cs
@@ -47,6 +47,58 @@ namespace BeautyForestAgent
         {
             VisibleChange(true);
         }
+
+        private void 끝내기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "종료";
+            Application.ExitThread();  
+        }
+
+        private void 수정ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+
+        }
+
+        private void ShowModal()
+        {
+            this.statusMessage.Text = "프로필 수정";
+            FormModify m = new FormModify();
+            m.Title = "프로필 수정";
+            m.ShowDialog();
+        }
+
+        private void BtnModify_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+        }
+
+        private void 추가ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModal2();
+        }
+
+        private void ShowModal2()
+        {
+            this.statusMessage.Text = "프로필 추가";
+            FormAdd m = new FormAdd();
+            m.Title = "프로필 추가";
+            m.ShowDialog();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            ShowModal2();
+        }
+
+        private void 이프로그램은ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "도움말 -> 이 프로그램은.. 창을 띄웁니다.";
+            FormAbout m = new FormAbout();
+            m.Title = "이 프로그램은.. 창 띄우기";
+            m.Show();
+
+        }
     }
 
 }
